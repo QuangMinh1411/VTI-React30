024632f9350ae935cdb50476bd7b95df5d7733ee
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2023-05-06 21:02:53 +0700

    Update

--- React/state-demo/src/App.jsx
+++ React/state-demo/src/App.jsx
@@ -2,20 +2,26 @@ import "./App.css";
 import Counter from "./Counter";
 import { useState } from 'react'
 function App() {
-  const [count,setCount] = useState(0)
-  
+  const [counter,setCount] = useState(0)
+  const [num,setNum] = useState(0);
     const increaseHandle = ()=>{
-        setCount(count+1);
+        setCount(counter+num);
     }
     const decreaseHandle = ()=>{
-        setCount(count-1)
+        setCount(counter-num)
     }
     const reset =()=>{
       setCount(0);
     }
+    const handleChange = (e)=>{
+      setNum(Number(e.target.value))
+    }
   return (
     <div className="App">
-      <Counter count={count} increase={increaseHandle} decrease={decreaseHandle} reset={reset} />
+      <Counter count={counter} 
+      increase={increaseHandle} decrease={decreaseHandle} 
+      reset={reset}
+      handleChange={handleChange} />
     </div>
   );
 }
--- React/state-demo/src/Counter.jsx
+++ React/state-demo/src/Counter.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Counter = ({count,increase,decrease,reset}) => {
-    
+const Counter = ({count,increase,decrease,reset,handleChange}) => {
+   
   return (
     <div>
       <div>{count}</div>
+      Add <input type='number' onChange={handleChange} />
+      <br />
       <button onClick={increase}>Increase</button>
       <button onClick={decrease}>Decrease</button>
       <button onClick={reset}>Reset</button>
