4583ad985430d9cb77ed8e1bb3d6cab05739d5cc
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2023-05-06 21:21:47 +0700

    Object state

--- React/state-demo/src/App.jsx
+++ React/state-demo/src/App.jsx
@@ -1,27 +1,29 @@
 import "./App.css";
 import Counter from "./Counter";
 import { useState } from 'react'
+import NewsletterSignup from "./Form/NewsLetterSignup";
 function App() {
-  const [counter,setCount] = useState(0)
-  const [num,setNum] = useState(0);
-    const increaseHandle = ()=>{
-        setCount(counter+num);
-    }
-    const decreaseHandle = ()=>{
-        setCount(counter-num)
-    }
-    const reset =()=>{
-      setCount(0);
-    }
-    const handleChange = (e)=>{
-      setNum(Number(e.target.value))
-    }
+  // const [counter,setCount] = useState(0)
+  // const [num,setNum] = useState(0);
+  //   const increaseHandle = ()=>{
+  //       setCount(counter+num);
+  //   }
+  //   const decreaseHandle = ()=>{
+  //       setCount(counter-num)
+  //   }
+  //   const reset =()=>{
+  //     setCount(0);
+  //   }
+  //   const handleChange = (e)=>{
+  //     setNum(Number(e.target.value))
+  //   }
   return (
     <div className="App">
-      <Counter count={counter} 
+      {/* <Counter count={counter} 
       increase={increaseHandle} decrease={decreaseHandle} 
       reset={reset}
-      handleChange={handleChange} />
+      handleChange={handleChange} /> */}
+      <NewsletterSignup />
     </div>
   );
 }
